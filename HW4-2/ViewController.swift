754ac145df42e79dbd3aca9e7b4b9ecb268396f0
пгпг <>
2023-04-04 23:36:18 +0600

    Initial Commit

---
+++ HW4-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4-2
+//
+//  Created by cpu on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
